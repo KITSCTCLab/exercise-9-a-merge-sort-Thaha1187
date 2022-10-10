02de6caf081abcaf14930b179d4f2beb71ed54dd
Thaha1187 <110010500+Thaha1187@users.noreply.github.com>
2022-10-10 21:32:18 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,32 @@
 from typing import List
+# Write code here
+def merge_sort(data):
+    if len(data)>1:
+        mid = len(data)//2
+        lefthalf = data[:mid]
+        righthalf = data[mid:]
 
-def merge_sort(data) -> None:
-  # Write code here
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+        i=j=k=0      
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                data[k]=lefthalf[i]
+                i=i+1
+            else:
+                data[k]=righthalf[j]
+                j=j+1
+            k=k+1
 
+        while i < len(lefthalf):
+            data[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            data[k]=righthalf[j]
+            j=j+1
+            k=k+1
 
 # Do not change the following code
 input_data = input()
@@ -13,4 +37,4 @@ for item in input_data.split(', '):
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
 merge_sort(data)
-print(data)
+print(data
